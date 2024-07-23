08c322758067360272c339eec70effff9074b93f
Taylor Cesarski <tccesars@ncsu.edu>
2024-07-23 15:28:26 -0400

    final changes hopefully

--- API.R
+++ API.R
@@ -4,13 +4,18 @@ library(readr)
 #Read in the data.
 api_diabetes_data <- read_csv("diabetes_binary_health_indicators_BRFSS2015.csv")
 
-#Convert all relevant predictors to factors.
-api_diabetes_data$Diabetes_binary <- as.factor(api_diabetes_data$Diabetes_binary)
-api_diabetes_data$HighBP <- as.factor(api_diabetes_data$HighBP)
-api_diabetes_data$HighChol <- as.factor(api_diabetes_data$HighChol)
-api_diabetes_data$HeartDiseaseorAttack <- as.factor(api_diabetes_data$HeartDiseaseorAttack)
-api_diabetes_data$GenHlth <- as.factor(api_diabetes_data$GenHlth)
-api_diabetes_data$DiffWalk <- as.factor(api_diabetes_data$DiffWalk)
+#Convert all relevant predictors to factors with relevant level names.
+api_diabetes_data$Diabetes_binary <- factor(api_diabetes_data$Diabetes_binary, levels = c(0, 1), labels = c("no_diabetes", "diabetes"))
+
+api_diabetes_data$HighBP <- factor(api_diabetes_data$HighBP, levels = c(0, 1), labels = c("no_hbp", "hbp"))
+
+api_diabetes_data$HighChol <- factor(api_diabetes_data$HighChol, levels = c(0, 1), labels = c("no_high_chol", "high_chol"))
+
+api_diabetes_data$HeartDiseaseorAttack <- factor(api_diabetes_data$HeartDiseaseorAttack, levels = c(0, 1), labels = c("no_hd_or_attack", "hd_or_attack"))
+
+api_diabetes_data$GenHlth <- factor(api_diabetes_data$GenHlth, levels = c(1, 2, 3, 4, 5), labels = c("excellent", "very_good", "good", "fair", "poor"))
+
+api_diabetes_data$DiffWalk <- factor(api_diabetes_data$DiffWalk, levels = c(0, 1), labels = c("no_difficulty", "difficulty"))
 
 #Use the glm function to indicated generalized linear model.
 #Using logistic model #3 with interactions between HighChol & HDorAttack and interaction between GenHlth & DiffWalk.
@@ -20,13 +25,26 @@ final_model_diabetes <- glm(Diabetes_binary ~ HighBP + HighChol*HeartDiseaseorAt
                             data = api_diabetes_data,
                             family = binomial)
 
-#When converting to as.numeric it converts to 1 and 2 so subtract 1. 
-#The means will be used to pass as the default values.
-meanHBP <- mean(as.numeric(api_diabetes_data$HighBP) - 1)
-meanChol <- mean(as.numeric(api_diabetes_data$HighChol) - 1)
-meanHD <- mean(as.numeric(api_diabetes_data$HeartDiseaseorAttack) - 1)
-meanGen <- mean(as.numeric(api_diabetes_data$GenHlth) - 1)
-meanWalk <- mean(as.numeric(api_diabetes_data$DiffWalk) - 1)
+#Print out information about the final model.
+final_model_diabetes
+
+#Determine most prevalent class of each categorical variable to use as defaults.
+
+#No high blood pressure is more prevalent than high blood pressure.
+table(api_diabetes_data$HighBP)
+
+#No high cholesterol is more prevalent than high cholesterol.
+table(api_diabetes_data$HighChol)
+
+#No heart disease or attack is more prevalent than heart disease or attack.
+table(api_diabetes_data$HeartDiseaseorAttack)
+
+#Very good is the most prevalent class of general health rating.
+table(api_diabetes_data$GenHlth)
+
+#No difficulty walking is more prevalent than difficulty walking.
+table(api_diabetes_data$DiffWalk)
+
 
 #Info endpoint
 #This endpoint doesn't take any parameters.
@@ -34,8 +52,8 @@ meanWalk <- mean(as.numeric(api_diabetes_data$DiffWalk) - 1)
 #* @get /info
 function(){
   info <- paste("Taylor Cesarski", 
-                        "https://tcesarski.github.io/ST558FinalProject/",
-                        sep = " ")
+                "https://tcesarski.github.io/ST558FinalProject/",
+                sep = " ")
   return(info)
 }
 
@@ -48,19 +66,14 @@ function(){
 #* @param GenHlth General Health Rating
 #* @param DiffWalk Difficulty Walking
 #* @get /pred
-#The default values are the means.
-function(HighBP = meanHBP, 
-         HighChol = meanChol, 
-         HeartDiseaseorAttack = meanHD,
-         GenHlth = meanGen, 
-         DiffWalk = meanWalk){
-  #Since the means are naturally not a level of the factor, first convert to an integer then convert to a factor so that it can be passed in the model.
-  HighBP <- as.factor(as.integer(HighBP))
-  HighChol <- as.factor(as.integer(HighChol))
-  HeartDiseaseorAttack <- as.factor(as.integer(HeartDiseaseorAttack))
-  GenHlth <- as.factor(as.integer(GenHlth))
-  DiffWalk <- as.factor(as.integer(DiffWalk))
- 
+#The default values are the most prevalent class of each.
+function(HighBP = "no_hbp", 
+         HighChol = "no_high_chol", 
+         HeartDiseaseorAttack = "no_hd_or_attack",
+         GenHlth = "very_good", 
+         DiffWalk = "no_difficulty"){
+  
+  
   #Create a data frame to pass to the predict function. 
   predictors_data <- data.frame(HighBP = HighBP,
                                 HighChol = HighChol,
@@ -78,11 +91,11 @@ function(HighBP = meanHBP,
 
 #Example calls to API: 
 
-#No HBP, no High Cholesterol, no difficulty walking, no heart disease, and poor health rating.
-#http://127.0.0.1:8000/pred?HighBP=0&HighChol=0&HeartDiseaseorAttack=0&GenHlth=5&DiffWalk=0
+#No HBP, no High Cholesterol, no heart disease, poor general health, and no difficulty walking.
+#http://127.0.0.1:8000/pred?HighBP=no_hbp&HighChol=no_high_chol&HeartDiseaseorAttack=no_hd_or_attack&GenHlth=poor&DiffWalk=no_difficulty
 
-#HBP, High Cholesterol, no heart disease or attack, very good general health, and no difficulty walking.
-#http://127.0.0.1:8000/pred?HighBP=1&HighChol=1&HeartDiseaseorAttack=0&GenHlth=2&DiffWalk=0
+#HBP, High Cholesterol, no heart disease or attack, fair general health, and difficulty walking.
+#http://127.0.0.1:8000/pred?HighBP=hbp&HighChol=high_chol&HeartDiseaseorAttack=no_hd_or_attack&GenHlth=fair&DiffWalk=difficulty
 
-#HBP, no high cholesterol, no heart disease or attack, fair general health, and no difficulty walking.
-#http://127.0.0.1:8000/pred?HighBP=1&HighChol=0&HeartDiseaseorAttack=0&GenHlth=4&DiffWalk=0
+#HBP, no high cholesterol, no heart disease or attack, excellent general health, and no difficulty walking.
+#http://127.0.0.1:8000/pred?HighBP=hbp&HighChol=no_high_chol&HeartDiseaseorAttack=no_hd_or_attack&GenHlth=excellent&DiffWalk=no_difficulty
